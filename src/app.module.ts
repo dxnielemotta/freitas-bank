9d930b1a303c9246f98d2de2dcee613af628f1cd
dxnielemotta <danielemottarocha@gmail.com>
2024-07-01 15:47:23 -0300

    corrigindo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ClientesService } from './cliente/cliente.service';
-import { ClientesController } from './cliente/cliente.controller';
+import { ClienteService } from './cliente/cliente.service';
+import { ClienteController } from './cliente/cliente.controller';
 import { ContaService } from './conta/conta.service';
 import { ContaController } from './conta/conta.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ClientesController, ContaController],
-  providers: [AppService, ClientesService, ContaService],
+  controllers: [AppController, ClienteController, ContaController],
+  providers: [AppService, ClienteService, ContaService],
 })
 export class AppModule {}
