25b80cbc38da311be71206c0bc7d7212c4609410
dxnielemotta <danielemottarocha@gmail.com>
2024-07-02 11:21:03 -0300

    atualizando app.modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { ClienteService } from './cliente/cliente.service';
 import { ClienteController } from './cliente/cliente.controller';
 import { ContaService } from './conta/conta.service';
 import { ContaController } from './conta/conta.controller';
+import { GerenteService } from './gerente/gerente.service';
+import { GerenteController } from './gerente/gerente.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ClienteController, ContaController],
-  providers: [AppService, ClienteService, ContaService],
+  controllers: [AppController, ClienteController, ContaController, GerenteController],
+  providers: [AppService, ClienteService, ContaService, GerenteService],
 })
 export class AppModule {}
