224efc9114e47bfed95ca53a6b20afc1ef7f684d
dxnielemotta <danielemottarocha@gmail.com>
2024-07-02 12:36:04 -0300

    add funcionalidades mudarTipoConta e fecharConta

--- src/gerente/gerente.controller.ts
+++ src/gerente/gerente.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post, Body, Param } from '@nestjs/common';
+import { Controller, Post, Body, Param, Put, Delete } from '@nestjs/common';
 import { GerenteService } from './gerente.service';
+import { TipoConta } from 'src/enums/tipo-conta.enum';
 
 @Controller('gerentes')
 export class GerenteController {
@@ -25,5 +26,43 @@ export class GerenteController {
       telefone,
       rendaSalarial,
     );
+  }
+
+  @Put(':id/mudar/:clienteID')
+  mudarTipoConta(
+    @Param('id') id: string,
+    @Param('clienteID') clienteID: string,
+    @Body('contaID') contaID: string,
+    @Body('novoTipo') novoTipo: TipoConta,
+  ): void {
+    const gerente = this.gerenteService.obterGerente(id);
+    const cliente = gerente.clientes.find((c) => c.id === clienteID);
+
+    if (!cliente) {
+      throw new Error('Cliente não encontrado para o gerente.');
+    }
+    const conta = cliente.contas.find((c) => c.id === contaID);
+    if (!conta) {
+      throw new Error('Conta não encontrada para o cliente.');
+    }
+    gerente.mudarTipoConta(cliente, conta, novoTipo);
+  }
+
+  @Delete(':id/excluir/:clienteID/:contaID')
+  fecharConta(
+    @Param('id') id: string,
+    @Param('clienteID') clienteID: string,
+    @Param('contaID') contaID: string,
+  ): void {
+    const gerente = this.gerenteService.obterGerente(id);
+    const cliente = gerente.clientes.find((c) => c.id === clienteID);
+    if (!cliente) {
+      throw new Error('Cliente não encontrado para o gerente.');
+    }
+    const conta = cliente.contas.find((c) => c.id === contaID);
+    if (!conta) {
+      throw new Error('Conta não encontrada para o cliente.');
+    }
+    gerente.fecharConta(cliente, conta);
   }
 }
