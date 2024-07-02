c4c2656e08a99d5bfb89dbe73f769deec65a6d66
dxnielemotta <danielemottarocha@gmail.com>
2024-07-02 12:33:13 -0300

    add funcionalidade mudarTipoConta e fecharConta

--- src/cliente/cliente.controller.ts
+++ src/cliente/cliente.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { ClienteService } from './cliente.service';
 import { Cliente } from './cliente.model';
+import { TipoConta } from 'src/enums/tipo-conta.enum';
 
 @Controller('clientes')
 export class ClienteController {
@@ -31,5 +40,32 @@ export class ClienteController {
       rendaSalarial,
       gerenteID,
     );
+  }
+
+  @Put(':id/mudar')
+  mudarTipoConta(
+    @Param('id') id: string,
+    @Body('contaID') contaID: string,
+    @Body('novoTipo') novoTipo: TipoConta,
+  ): void {
+    const cliente = this.clienteService.obterCliente(id);
+    const conta = cliente.contas.find((c) => c.id === contaID);
+    if (!conta) {
+      throw new Error('Conta não encontrada para o cliente.');
+    }
+    cliente.mudarTipoConta(conta, novoTipo);
+  }
+
+  @Delete(':id/excluir/:contaID')
+  fecharConta(
+    @Param('id') id: string,
+    @Param('contaID') contaID: string,
+  ): void {
+    const cliente = this.clienteService.obterCliente(id);
+    const conta = cliente.contas.find((c) => c.id === contaID);
+    if (!conta) {
+      throw new Error('Conta não encontrada para o cliente.');
+    }
+    cliente.fecharConta(conta);
   }
 }
