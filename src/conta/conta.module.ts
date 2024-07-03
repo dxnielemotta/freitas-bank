cf19eeba8804eba2366a866af7b44b321e9b6c22
dxnielemotta <danielemottarocha@gmail.com>
2024-07-02 21:15:18 -0300

    corrigindo import

--- src/conta/conta.module.ts
+++ src/conta/conta.module.ts
@@ -1,10 +1,10 @@
 import { ClienteModule } from 'src/cliente/cliente.module';
 import { ContaController } from './conta.controller';
 import { ContaService } from './conta.service';
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 
 @Module({
-  imports: [ClienteModule],
+  imports: [forwardRef(() => ClienteModule)],
   controllers: [ContaController],
   providers: [ContaService],
   exports: [ContaService],
