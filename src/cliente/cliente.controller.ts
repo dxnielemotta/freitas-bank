6d89363a3270a57f563691cb99a57e2aa9682983
dxnielemotta <danielemottarocha@gmail.com>
2024-07-02 21:13:04 -0300

    add  cadastrarCliente,mudarTipoContae fecharConta

--- src/cliente/cliente.controller.ts
+++ src/cliente/cliente.controller.ts
@@ -71,4 +71,85 @@ export class ClienteController {
       throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
     }
   }
+
+  @Post('cadastrar')
+  cadastrarCliente(
+    @Body('nomeCompleto') nomeCompleto: string,
+    @Body('endereco') endereco: string,
+    @Body('telefone') telefone: string,
+    @Body('rendaSalarial') rendaSalarial: number,
+    @Body('gerenteID') gerenteID: string,
+  ) {
+    try {
+      const cliente = this.clienteService.cadastrarCliente(
+        nomeCompleto,
+        endereco,
+        telefone,
+        rendaSalarial,
+        gerenteID,
+      );
+      return {
+        statusCode: HttpStatus.CREATED,
+        message: 'Cliente cadastrado com sucesso',
+        data: cliente,
+      };
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  @Put(':id/mudar')
+  mudarTipoConta(
+    @Param('id') id: string,
+    @Body('contaID') contaID: string,
+    @Body('novoTipo') novoTipo: TipoConta,
+  ) {
+    try {
+      const cliente = this.clienteService.obterCliente(id);
+      if (!cliente) {
+        throw new HttpException('Cliente não encontrado', HttpStatus.NOT_FOUND);
+      }
+      const conta = cliente.contas.find((c) => c.id === contaID);
+      if (!conta) {
+        throw new HttpException(
+          'Conta não encontrada para o cliente',
+          HttpStatus.NOT_FOUND,
+        );
+      }
+
+      cliente.mudarTipoConta(conta, novoTipo);
+
+      return {
+        statusCode: HttpStatus.OK,
+        message: 'Mudança de conta feita com sucesso',
+      };
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  @Delete(':id/excluir/:contaID')
+  fecharConta(@Param('id') id: string, @Param('contaID') contaID: string) {
+    try {
+      const cliente = this.clienteService.obterCliente(id);
+      if (!cliente) {
+        throw new HttpException('Cliente não encontrado', HttpStatus.NOT_FOUND);
+      }
+      const conta = cliente.contas.find((c) => c.id === contaID);
+      if (!conta) {
+        throw new HttpException(
+          'Conta não encontrada para o cliente',
+          HttpStatus.NOT_FOUND,
+        );
+      }
+
+      cliente.fecharConta(conta);
+      return {
+        statusCode: HttpStatus.NO_CONTENT,
+        message: 'Conta fechada com sucesso',
+      };
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
+  }
 }
