c7a269f34bd708cc60c5a540e516c2d87bdcf765
dxnielemotta <danielemottarocha@gmail.com>
2024-07-04 10:24:45 -0300

    add app controllers e app providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,12 @@ import { Module } from '@nestjs/common';
 import { ClienteModule } from './cliente/cliente.module';
 import { GerenteModule } from './gerente/gerente.module';
 import { ContaModule } from './conta/conta.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [ClienteModule, GerenteModule, ContaModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
