9852c7e5dc92ea0082feb480ef05d0cc99257925
dxnielemotta <danielemottarocha@gmail.com>
2024-07-11 17:41:09 -0300

    corrigindo método de remover cliente

--- src/gerente/gerente.model.ts
+++ src/gerente/gerente.model.ts
@@ -1,4 +1,4 @@
-import { Cliente } from '../cliente/cliente.model';
+import { Cliente } from 'src/cliente/cliente.model';
 import { v4 as uuidv4 } from 'uuid';
 export class Gerente {
   id: string;
@@ -8,11 +8,16 @@ export class Gerente {
     this.id = uuidv4();
   }
 
-  adicionarCliente(cliente: Cliente): void {
-    this.clientes.push(cliente);
+  adicionarCliente(cliente: Cliente) {
+    return this.clientes.push(cliente);
   }
 
-  removerCliente(cliente: Cliente): void {
-    this.clientes = this.clientes.filter((item) => item.id !== cliente.id);
+  removerCliente(cliente: Cliente) {
+    const index = this.clientes.findIndex((item) => item.id === cliente.id);
+    if (index < 0) {
+      throw new Error('Cliente não encontrado');
+    }
+
+    return this.clientes.splice(index, 1);
   }
 }
