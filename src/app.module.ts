145ea09986936567dc060df2a6f79da0b11f741f
dxnielemotta <danielemottarocha@gmail.com>
2024-07-27 19:01:00 -0300

    configurando banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,27 @@ import { GerenteModule } from './gerente/gerente.module';
 import { ContaModule } from './conta/conta.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+//mudar de model para entity
+import { Gerente } from './gerente/gerente.model';
+import { Cliente } from './cliente/cliente.entity';
 
 @Module({
-  imports: [ClienteModule, GerenteModule, ContaModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'freitas-bank',
+      username: 'postgres',
+      password: 'postgres',
+      entities: [Cliente, Gerente],
+      synchronize: true,
+    }),
+    ClienteModule,
+    GerenteModule,
+    ContaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
